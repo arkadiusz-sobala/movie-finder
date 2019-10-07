4cdbd37e36ae053e3577e7ba8f3a8fc2f719d210
Arkadiusz <arkadiusz.sobala1995@gmail.com>
2019-10-07 22:49:31 +0200

    Nowy komponent, stylowanie inputa i pobieranie filmów

--- panel/src/App.js
+++ panel/src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import CustomSelect from "./custom-select/custom-select";
 
 class App extends Component {
-    handle = () => {
-        axios
-            .get("http://www.omdbapi.com/?apikey=123497db&s=pirates+of&page=5")
-            .then(response => {
-                console.log(response);
-            });
+    state = {
+        chosenMovie: undefined,
+        searchFraze: "",
+        foundMovies: [],
+        searchPlaceholder: "Search movies..."
     };
+
+    handleSearchFrazeChange = event => {
+        this.setState({ searchFraze: event.target.value });
+        this.searchMovies(event.target.value);
+    };
+
+    searchMovies(searchFraze) {
+        let noSpacesSearchFraze = searchFraze.replace(/\s/g, "+");
+        axios
+            .get(
+                `http://www.omdbapi.com/?apikey=123497db&s=${noSpacesSearchFraze}`
+            )
+            .then(response => {
+                if (response.data && response.data.Search)
+                    this.setState({
+                        foundMovies: response.data.Search.splice(0, 5)
+                    });
+                else if (response.data.Error)
+                    this.setState({
+                        foundMovies: [
+                            { error: true, message: response.data.Error }
+                        ]
+                    });
+                else this.setState({ foundMovies: [] });
+            });
+    }
+
     render() {
-        this.handle();
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
+            <div className="app-background">
+                <div className="app-container">
+                    <CustomSelect
+                        value={this.state.searchFraze}
+                        placeholder={this.state.searchPlaceholder}
+                        foundMovies={this.state.foundMovies}
+                        foundMovies={this.state.foundMovies}
+                        valueChange={() => this.handleSearchFrazeChange(event)}
+                    ></CustomSelect>
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
-                    reload.
-                </p>
             </div>
         );
     }
